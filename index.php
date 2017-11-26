cec21fb8a0a930678ff5f6acddaf82617186c96c
Jeremy Ward <jeremy.ward@webdevstudios.com>
2017-11-26 13:45:25 -0600

    Increase font size, add style and hover effect to inline links.

--- index.php
+++ index.php
@@ -7,5 +7,16 @@
 
 <?php get_header(); ?>
 
+<main class="site-main">
+	<?php if ( have_posts() ) : ?>
+		<?php while ( have_posts() ) : the_post(); ?>
+			<article <?php post_class(); ?>>
+				<div class="entry__content">
+					<?php the_content(); ?>
+				</div>
+			</article>
+		<?php endwhile; ?>
+	<?php endif; ?>
+</main>
 
 <?php get_footer(); ?>
