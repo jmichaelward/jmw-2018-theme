6a9656651ed58be45cc2594212844f9a4c3a4d7b
Jeremy Ward <jeremy.ward@webdevstudios.com>
2017-11-26 19:08:52 -0600

    Add viewport information to header.

--- header.php
+++ header.php
@@ -7,6 +7,8 @@
 <!DOCTYPE html>
 <html <?php language_attributes(); ?>>
 	<head>
+		<meta charset="<?php bloginfo( 'charset' ); ?>">
+		<meta name="viewport" content="width=device-width">
 		<?php wp_head(); ?>
 	</head>
 	<body <?php body_class(); ?>>
